13e4fb64ef8cee5172e7e02bc135f06943a6f3f7
Franco Angulo <francoangulo2001@gmail.com>
2022-05-23 14:01:25 -0300

    added footer

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Navbar from "./components/Navbar";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
     return (
@@ -17,6 +18,7 @@ function App() {
             <Projects />
             <Skills />
             <Contact />
+            <Footer />
         </Stack>
     );
 }
